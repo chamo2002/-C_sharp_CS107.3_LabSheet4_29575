8bc1ffe4ea6875054f2374b1691234e2db9458c4
chamo <137000540+chamo2002@users.noreply.github.com>
2024-04-11 00:08:54 +0530

    Add files via upload

---
+++ Lab4 Question3/Lab4 Question3/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Lab4_Question3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtnum = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.lblanswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculator";
+            // 
+            // txtnum
+            // 
+            this.txtnum.Location = new System.Drawing.Point(114, 82);
+            this.txtnum.Name = "txtnum";
+            this.txtnum.Size = new System.Drawing.Size(379, 20);
+            this.txtnum.TabIndex = 1;
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(115, 136);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(90, 33);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "+";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(211, 136);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(90, 33);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = "-";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(307, 136);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(90, 33);
+            this.btn3.TabIndex = 4;
+            this.btn3.Text = "*";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(403, 136);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(90, 33);
+            this.btn4.TabIndex = 5;
+            this.btn4.Text = "/";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(211, 198);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(186, 33);
+            this.btn5.TabIndex = 6;
+            this.btn5.Text = "=";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // lblanswer
+            // 
+            this.lblanswer.AutoSize = true;
+            this.lblanswer.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblanswer.Location = new System.Drawing.Point(228, 249);
+            this.lblanswer.Name = "lblanswer";
+            this.lblanswer.Size = new System.Drawing.Size(0, 36);
+            this.lblanswer.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblanswer);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txtnum);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtnum;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Label lblanswer;
+    }
+}
+
---
+++ Lab4 Question3/Lab4 Question3/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lab4_Question3
+{
+    public partial class Form1 : Form
+    {
+        public int fnum;
+        public int snum;
+        public int answer;
+        public string operation = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+           fnum = Convert.ToInt32(txtnum.Text);
+
+            txtnum.Text = "";
+
+            operation = "+";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            fnum = Convert.ToInt32(txtnum.Text);
+
+            txtnum.Text = "";
+
+            operation = "-";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            fnum = Convert.ToInt32(txtnum.Text);
+
+            txtnum.Text = "";
+
+            operation = "*";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            fnum = Convert.ToInt32(txtnum.Text);
+
+            txtnum.Text = "";
+
+            operation = "/";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+
+            snum = Convert.ToInt32(txtnum.Text);
+
+            switch (operation)
+            {
+                case "+":
+                    answer = fnum + snum;
+                    break;
+                case "-":
+                    answer = fnum - snum;
+                    break;
+                case "*":
+                    answer = fnum * snum;
+                    break;
+                case "/":
+                    answer = fnum / snum;
+                    break;
+            }
+
+            txtnum.Text = answer.ToString();
+        }
+
+    }
+}
+
