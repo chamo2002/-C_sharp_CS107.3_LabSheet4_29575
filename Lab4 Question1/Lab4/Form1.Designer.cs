489a8b48703f4bf819a579772ec92339f6ca4c0d
chamo <137000540+chamo2002@users.noreply.github.com>
2024-04-10 12:38:57 +0530

    Add files via upload

---
+++ Lab4 Question1/Lab4/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtfnum = new System.Windows.Forms.TextBox();
+            this.txtsnum = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.lblanswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(91, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Frist Number:";
+            // 
+            // txtfnum
+            // 
+            this.txtfnum.Location = new System.Drawing.Point(220, 38);
+            this.txtfnum.Name = "txtfnum";
+            this.txtfnum.Size = new System.Drawing.Size(100, 20);
+            this.txtfnum.TabIndex = 1;
+            // 
+            // txtsnum
+            // 
+            this.txtsnum.Location = new System.Drawing.Point(233, 92);
+            this.txtsnum.Name = "txtsnum";
+            this.txtsnum.Size = new System.Drawing.Size(100, 20);
+            this.txtsnum.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(91, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Second Number:";
+            // 
+            // btnadd
+            // 
+            this.btnadd.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnadd.Location = new System.Drawing.Point(167, 147);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(97, 28);
+            this.btnadd.TabIndex = 4;
+            this.btnadd.Text = "ADD";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // lblanswer
+            // 
+            this.lblanswer.AutoSize = true;
+            this.lblanswer.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblanswer.Location = new System.Drawing.Point(164, 211);
+            this.lblanswer.Name = "lblanswer";
+            this.lblanswer.Size = new System.Drawing.Size(0, 26);
+            this.lblanswer.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblanswer);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.txtsnum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtfnum);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtfnum;
+        private System.Windows.Forms.TextBox txtsnum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Label lblanswer;
+    }
+}
+
---
+++ Lab4 Question1/Lab4/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        public int  fnum, snum, answer;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            fnum = Convert.ToInt32(txtfnum.Text);
+            snum = Convert.ToInt32(txtsnum.Text);
+
+            answer = fnum + snum;
+
+            lblanswer.Text = answer.ToString();
+        }
+    }
+}
